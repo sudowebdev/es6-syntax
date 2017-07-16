0ec0333e9a1da366e1d72aa0b9edbb7509890a0c
Nikhil Arora <narora200@gmail.com>
2017-07-16 11:23:14 +0530

    var: Definition

--- var, let and const/var.js
+++ var, let and const/var.js
@@ -9,4 +9,15 @@ for (var i = 0; i < 10; i++) {
 console.log('after loop', i);
 
 
+/*
+	Error in the following code because of the functional scope of var
+*/
+function count() {
+	for (var j = 0; j < 10; j++) {
 
+		console.log(j);
+	}
+}
+
+count();
+console.log('after function', j);
