500a561ce1b5ca47ab48d245c3a9e2f77b47673a
Nikhil Arora <narora200@gmail.com>
2017-07-16 14:25:27 +0530

    const: Another use case

--- var, let and const/const.js
+++ var, let and const/const.js
@@ -10,3 +10,11 @@ const a = 3;
 const anotherObject = {name: "newObject"};
 //anotherObject = {name: "anotherNewObject"};
 console.log(anotherObject);
+
+
+/*
+	But it lets you to modify the object properties
+*/
+const person = {name: "sudowebdev"};
+person.name = "thesudocode";
+console.log(person);
