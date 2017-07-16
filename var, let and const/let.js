9d35e9c85f758655d08e42e6bcc3d400369288d9
Nikhil Arora <narora200@gmail.com>
2017-07-16 11:31:08 +0530

    let: let Comparison with var

--- var, let and const/let.js
+++ var, let and const/let.js
@@ -8,4 +8,16 @@ for (let j = 0; j < 10; j++) {
 }
 
 // j comes to be not defined because of the block-level scope of let
-console.log('after loop', j);
+//console.log('after loop', j);
+
+
+/*
+	Using var in the same context doesn't give any error
+*/
+for (var i = 0; i < 10; i++) {
+
+	console.log(i);
+}
+
+
+console.log('after loop', i);
