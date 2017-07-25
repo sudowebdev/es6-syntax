6f40be1e4036c3a9e716a580104188102ec3c029
Nikhil Arora <narora200@gmail.com>
2017-07-25 23:40:56 +0530

    Destructuring: Use-case (Part 3: Normal Implementation (neater implementation)

--- destructuring/use-case.js
+++ destructuring/use-case.js
@@ -12,8 +12,15 @@ makeSound({
 
 //Normal implementation
 function makeSound(options){
-	options.species = options.species || 'animal';
-	console.log("The " + options.species + " makes a " + options.sound + " sound!");
+	/*
+		options.species = options.species || 'animal';
+		console.log("The " + options.species + " makes a " + options.sound + " sound!");
+	*/
+
+	//to make it more viable
+	let species = options.species || 'animal';
+	let sound = options.sound;
+	console.log("The " + species + " makes a " + sound + " sound!");
 
 	//With species: The dog makes a woof sound!
 	//Without species: The animal makes a woof sound!
