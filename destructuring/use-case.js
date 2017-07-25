556d15341bb9a7f973ce3e513df701bcc25e5c7c
Nikhil Arora <narora200@gmail.com>
2017-07-25 23:37:37 +0530

    Destructuring: Use-case (Part 2: Normal Implementation (with species property provided)

--- destructuring/use-case.js
+++ destructuring/use-case.js
@@ -9,3 +9,11 @@ makeSound({
 	We have to implement that function with a restriction to 
 	make the species property optional 
 */ 
+
+function makeSound(options){
+	options.species = options.species || 'animal';
+	console.log("The " + options.species + " makes a " + options.sound + " sound!");
+
+	//With species: The dog makes a woof sound!
+
+}
